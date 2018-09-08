3900ef0fdc0fe0cefed86cc99df6dd7820f19935
Steve <stevencaplette@gmail.com>
2018-09-08 12:55:49 -0400

    simplify to just security log events

--- EventLogHandler/Program.cs
+++ EventLogHandler/Program.cs
@@ -3,6 +3,8 @@ using System.Xml;
 using System.Diagnostics.Eventing.Reader;
 using System.Text.RegularExpressions;
 using IPInfo = System.Collections.Concurrent.ConcurrentDictionary<System.String, System.Collections.Generic.List<EventLogHandler.InterestingSecurityFailure>>;
+using System.Collections.Generic;
+using System.Linq;
 
 class EventLogHandler
 {
@@ -12,32 +14,15 @@ class EventLogHandler
         // Subscribing to event log 
         // additions
 
-        EventLogWatcher tslogwatcher = null;
         EventLogWatcher seclogwatcher = null;
 
         try
         {
-            // Subscribe to receive event notifications
-            // in the RDP event log. The query specifies
-            // that only EventID 140 events will be returned.
-            var subquery = new EventLogQuery(
-                "Microsoft-Windows-RemoteDesktopServices-RdpCoreTS/Operational", PathType.LogName, "*[System/EventID=140]");
-
-            tslogwatcher = new EventLogWatcher(subquery);
-
-            // Set watcher to listen for the EventRecordWritten
-            // event.  When this event happens, the callback method
-            // (EventLogEventRead) will be called.
-            tslogwatcher.EventRecordWritten +=
-                new EventHandler<EventRecordWrittenEventArgs>(
-                    EventLogEventRead);
-
-            // Begin subscribing to events the events
-            tslogwatcher.Enabled = true;
-
-            // Also subscribe to security event log
+            // Subscribe to security event log
             // Logon failure events
-            subquery = new EventLogQuery("Security", PathType.LogName, "*[System/EventID=4625]");
+            var subquery = new EventLogQuery("Security",
+                PathType.LogName, "*[System/EventID=4625]");
+
             seclogwatcher = new EventLogWatcher(subquery);
             seclogwatcher.EventRecordWritten +=
                 new EventHandler<EventRecordWrittenEventArgs>(
@@ -47,19 +32,9 @@ class EventLogHandler
             WriteInfo("Waiting for events...");
             Console.ReadLine();
         }
-        catch (EventLogReadingException e)
-        {
-            WriteInfo($"Error reading the log: {e.Message}");
-        }
         finally
         {
            
-            if (tslogwatcher != null)
-            {
-                // Stop listening to events
-                tslogwatcher.Enabled = false;
-                tslogwatcher.Dispose();
-            }
             if (seclogwatcher != null)
             {
                 // Stop listening to events
@@ -71,7 +46,7 @@ class EventLogHandler
 
     public static void WriteInfo(string infomessage)
     {
-        Console.WriteLine($"[{DateTime.Now.ToString("o")}] {infomessage}");
+        Console.WriteLine($"[{DateTime.Now.ToString("o").Replace('T',' ')}] {infomessage}");
     }
 
     /// <summary>
@@ -84,56 +59,35 @@ class EventLogHandler
         // Make sure there was no error reading the event.
         if (arg.EventRecord != null)
         {
-
             ProcessEvent(arg.EventRecord);         
-
-        }
-        else
-        {
-            WriteInfo("The event instance was null.");
         }
     }
     public static void ProcessEvent(EventRecord er) {
 
-        WriteInfo($"Received event {er.Id} from the subscription.");
-        WriteInfo($"Description: {er.FormatDescription()}");
-        WriteInfo($"Event XML: {er.ToXml()}");
-        var xml = new XmlDocument();
-        xml.LoadXml(er.ToXml().Replace("xmlns='http://schemas.microsoft.com/win/2004/08/events/event'", ""));
+        WriteInfo($"Received event from the subscription.");
 
-        var ie = new InterestingSecurityFailure
+        var xml = new XmlDocument();
+        xml.LoadXml(er.ToXml());
+        var ns = new XmlNamespaceManager(xml.NameTable);
+        ns.AddNamespace("a", "http://schemas.microsoft.com/win/2004/08/events/event");
+
+        var isf = new InterestingSecurityFailure
         {
-            Date = er.TimeCreated
+            Date = er.TimeCreated,
+            Ip = (xml.SelectSingleNode("//a:Data[@Name=\"IpAddress\"]", ns)).InnerText,
+            UserName = (xml.SelectSingleNode("//a:Data[@Name=\"TargetUserName\"]", ns))?.InnerText,
+            Domain = (xml.SelectSingleNode("//a:Data[@Name=\"TargetDomainName\"]", ns))?.InnerText
         };
 
-        switch (er.Id)
+        if (isLocalAddress(isf.Ip))
         {
-            case 140:
-                ProcessEvent140(xml,ie);
-                break;
-            case 4625:
-                ProcessEvent4625(xml,ie);
-                break;
-            
+            WriteInfo("Local address found. Skipping.");
+            return;
         }
 
-        if (isLocalAddress(ie.Ip)) { return; }
-
-        blockIpIfNecessary(ie.Ip);
+        blockIpIfNecessary(isf);
     }
 
-
-    private static void ProcessEvent4625(XmlDocument xml, InterestingSecurityFailure ie)
-    {
-        ie.Ip = (xml.SelectSingleNode("//Data[@Name=\"IpAddress\"]"))?.InnerText;
-        ie.UserName = (xml.SelectSingleNode("//Data[@Name=\"TargetUserName\"]"))?.InnerText;
-        ie.Domain = (xml.SelectSingleNode("//Data[@Name=\"TargetDomainName\"]"))?.InnerText;
-    }
-
-    private static void ProcessEvent140(XmlDocument xml, InterestingSecurityFailure ie)
-    {
-        ie.Ip = (xml.SelectSingleNode("//Data[@Name=\"IPString\"]"))?.InnerText;
-    }
 
     public static bool isLocalAddress(string ip)
     {
@@ -142,14 +96,30 @@ class EventLogHandler
 
     }
 
-    public static void blockIpIfNecessary(string ip)
+    public static void blockIpIfNecessary(InterestingSecurityFailure isf)
     {
+        if (!_ipdeets.ContainsKey(isf.Ip))
+        {
+            WriteInfo($"Adding record to dictionary for {isf.Ip}");
+            _ipdeets[isf.Ip] = new List<InterestingSecurityFailure>();
+        }
+        _ipdeets[isf.Ip].Add(isf);
+        var failures = _ipdeets[isf.Ip].Count(e => e.Date > DateTime.Now.AddHours(-24));
+        WriteInfo($"IP [{isf.Ip}]. Failure count in last 24 hours [{failures}].");
+        WriteInfo($"User [{isf.UserName}]. Domain [{isf.Domain}].");
+        if (failures > 1)
+        {
+            WriteInfo($"Need to block ip {isf.Ip}");
+        }
+
 
     }
+
     // TODO: Generalize for private IP ranges
     // instead of my house's range.
     private static Regex _localAddressRE = new Regex(@"192\.168\.[0-5]\.\d{1,3}",
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
     private static IPInfo _ipdeets = new IPInfo();
 
     public class InterestingSecurityFailure
