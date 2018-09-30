a895b8aa6b53911323971bbaac9b81c00f43f962
Steve <stevencaplette@gmail.com>
2018-09-30 13:14:29 -0400

    Powershell script now handles rule creation if necessary.

--- AdaptiveFirewallService.exe/Resources/PowerShell/BlockIp.ps1
+++ AdaptiveFirewallService.exe/Resources/PowerShell/BlockIp.ps1
@@ -11,11 +11,45 @@ function Info {
     Param($InfoMessage)
         Write-Information "{PowerShell script} $InfoMessage"
 }
-
+$FirewallRuleName = "AdaptiveFirwallService-Block RDP bots"
+$CreateRule = $false
 $InformationPreference = 'Continue'
 
-Info "Getting exising blocked IPs from firewall rule (Block RDP Bots)"
-$filter = Get-NetFirewallRule -DisplayName "Block RDP Bots" -ErrorAction Stop | Get-NetFirewallAddressFilter
+try {
+    Info "Getting exising blocked IPs from firewall rule [$FirewallRuleName]"
+    $rule = Get-NetFirewallRule -DisplayName $FirewallRuleName -ErrorAction Stop
+}
+catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException]{
+
+   if ($_.Exception.Message -like 'No MSFT_NetFirewallRule objects found with property*')
+   {
+       $CreateRule = $true
+   }
+   else {
+       throw
+   }
+}
+
+if ($CreateRule)
+{
+    Info "Firewall rule not found. Creating new."
+    # rule not found, create it
+    $createArgs = @{
+        DisplayName = $FirewallRuleName;
+        Direction = 'Inbound';
+        Action = 'Block';
+        Enabled = 'False';
+    }
+
+    $rule = New-NetFirewallRule @createArgs
+    $filter = $rule | Get-NetFirewallAddressFilter
+    Set-NetFirewallAddressFilter -InputObject $filter -RemoteAddress $IpAddress
+    Set-NetFirewallRule -InputObject $rule -Enabled True
+    Info "   done."
+    return
+}
+
+$filter = $rule | Get-NetFirewallAddressFilter
 $existingIps = $filter.RemoteAddress
 Info "   found [$($existingIps.Count)] IPs"
 Info "   done."
@@ -27,5 +61,5 @@ $existingIps |
 }
 $distinctIps.Add($IpAddress) | Out-Null
 if ($distinctIps.Count -gt $existingIps.Count) {
-        Set-NetFirewallAddressFilter -InputObject $filter -RemoteAddress $distinctIps
+    Set-NetFirewallAddressFilter -InputObject $filter -RemoteAddress $distinctIps
 }
