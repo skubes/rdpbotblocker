efebf697242b7d639d435881fff52f76c071e70a
Steve <stevencaplette@gmail.com>
2018-10-08 19:50:41 -0400

    update installer build script to find Inno Setup install location from registry

--- Install/BuildInstaller.ps1
+++ Install/BuildInstaller.ps1
@@ -3,6 +3,24 @@ param(
 	[ValidateSet("Debug","Release")]
 	$BuildConfiguration
 )
+
 $scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$iscc = "C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
+
+# Get Inno Setup install folder from registry (assumes x64 Windows)
+try {
+	$installDir = (Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Inno Setup 5_is1" -Name InstallLocation -ErrorAction Stop).InstallLocation
+}
+catch {
+	Write-Error -ErrorRecord $_
+	Write-Output "Couldn't detect Inno Setup installed on this system."
+	Write-Output "If it's not installed, run innosetup-qsp-5.6.1.exe (in folder $scriptFolder)"
+	Write-Output "or go to: http://www.jrsoftware.org/isdl.php"
+	Write-Output "The .iss file was tested with Inno Setup v5.6.1"
+	return
+}
+
+$iscc = Join-Path $installDir "ISCC.exe"
+
+# call ISCC console compiler. (This also requires Inno Preprocessor
+# to be installed to support defining BuildConfiguration variable here.)
 &$iscc "/DBuildConfiguration=$BuildConfiguration" $scriptFolder\AdaptiveFirewall.iss
