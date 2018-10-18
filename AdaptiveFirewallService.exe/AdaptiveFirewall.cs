b1e0a5caa7a287a544f63db763baf13830bb0d6c
Steve <35824205+skubes@users.noreply.github.com>
2018-10-17 22:29:24 -0400

    Revert "another ci test"
    This reverts commit 92a8d6419e952226a4d22cbcbf5a41ba4b77356f.

--- AdaptiveFirewallService.exe/AdaptiveFirewall.cs
+++ AdaptiveFirewallService.exe/AdaptiveFirewall.cs
@@ -204,7 +204,7 @@ namespace SCAdaptiveFirewall
             var xml = new XmlDocument();
             xml.LoadXml(eventRecord.ToXml());
             var ns = new XmlNamespaceManager(xml.NameTable);
-            ns.AddNamespace("a", "http://schemas.microsoft.com/win/2004/08/events/evnt");
+            ns.AddNamespace("a", "http://schemas.microsoft.com/win/2004/08/events/event");
 
             var isf = new InterestingSecurityFailure
             {
